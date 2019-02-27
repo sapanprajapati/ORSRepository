6d2bc57542eeea64e77269ffb0aff3642bf19f0b
sapan kumar prajapati <sapan.prajapati@gmail.com>
2019-02-27 16:51:46 +0530

    Added log4j2 for logging activities

--- ors-app/src/main/java/com/bhagat/hritu/controller/LoginController.java
+++ ors-app/src/main/java/com/bhagat/hritu/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.bhagat.hritu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +16,18 @@ import com.bhagat.hritu.service.UserService;
 public class LoginController {
     @Autowired
     private UserService userService;
-
+    
+    private static Logger LOG= LoggerFactory.getLogger(LoginController.class);
     @RequestMapping("/")
     public String displayLogin() {
+        LOG.info("diplay login method start");
         return "login";
     }
 
     @RequestMapping(
             value = "/login.do", method = RequestMethod.POST)
-    public String authenticateUser(@ModelAttribute UserDTO user, Model model) {
+    public String authenticate(@ModelAttribute UserDTO user, Model model) {
+        LOG.info("authenticate method start");
         UserDTO userDto = userService.authenticateUser(user);
         if(null !=userDto){
             model.addAttribute("fname", userDto.getFname());
@@ -30,6 +35,7 @@ public class LoginController {
             return "welcome";
         }
         model.addAttribute("errorMessage", "User Not Found!!");
+        LOG.info("authenticate method end");
         return "login";
     }
 }
